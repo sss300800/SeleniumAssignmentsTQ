f87408acc29d1b149ebf21ef9ba858e6c579e535
Somnath Sopan Shirsath <83588073+sss300800@users.noreply.github.com>
2021-09-15 21:40:05 +0530

    Add files via upload

---
+++ ByAbsoluteXpath.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WebDriverAssignment2
+{
+    class ByAbsoluteXpath
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver("C:\\Users\\icon\\source\\repos");
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.google.co.in/";
+            driver.FindElement(By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")).SendKeys("selenium");
+            //driver.Close();
+
+
+
+        }
+    }
+}
+
+
---
+++ ByRelativeXpath.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+
+namespace WebDriverAssignment2
+{
+    class ByRelativeXpath
+    {
+        static void main(string[] args)
+        {
+            IWe
+        }bDriver driver = new ChromeDriver("C:\\Users\\icon\\source\\repos");
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.google.co.in/";
+            driver.FindElement(By.XPath("//div[@class=gLFyf gsfi")).SendKeys("selenium");
+            driver.Close();
+    }
+}
