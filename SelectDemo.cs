85b988f773524b0c894f103a54d08061d0712605
Somnath Sopan Shirsath <83588073+sss300800@users.noreply.github.com>
2021-09-17 15:42:40 +0530

    Add files via upload

---
+++ SelectDemo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SelectDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver("C:\\Users\\icon\\source\\repos");
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://formstone.it/components/dropdown/demo/";
+            IWebElement select_box = driver.FindElement(By.Id("demo_basic"));
+            SelectElement select = new SelectElement(select_box);
+            select.SelectByIndex(1);   //starting from zero.
+            //select.SelectByText("Two");
+            //select.SelectByValue("1");
+            driver.Close();
+
+        }
+    }
+}
